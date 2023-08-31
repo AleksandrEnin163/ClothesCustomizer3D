8eb6352e62968d671e006782e9b324fc52efcf9a
AleksandrEnin163 <alexenin163@gmail.com>
2023-08-31 14:03:32 +0200

    1st commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {motiin, AnimatePresence} from 'framer-motion'
+import {useSnapshot} from 'valtio'
+
+import state from '../store';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
+
+function Home() {
+  const snap = useSnapshot(state)
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
