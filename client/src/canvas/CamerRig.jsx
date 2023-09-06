8d69535daec2c0d71cfad968752109017250bf15
AleksandrEnin163 <alexenin163@gmail.com>
2023-09-06 13:43:03 +0200

    canvas

---
+++ client/src/canvas/CamerRig.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CamerRig() {
+  return (
+    <div>CamerRig</div>
+  )
+}
+
+export default CamerRig
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Canvas} from '@react-three/fiber'
+import {Environment, Center} from '@react-three/drei'
+import Backdrop from './Backdrop'
+import CamerRig from './CamerRig'
+import Shirt from './Shirt'
 
-function Canvas() {
+function CanvasModel() {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+
+      {/* <CamerRig> */}
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      {/* </CamerRig> */}
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
